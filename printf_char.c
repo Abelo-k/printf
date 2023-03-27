b40e6a4fc01273778c6065ea779d1e34b12c0a2e
Simon-Asfaw <simonasfaw587@gmail.com>
2023-03-27 16:50:39 +0000

    pritf_char

---
+++ printf_char.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * printf_char - prints char
+ * @val: arguments
+ * Return: 1
+ */
+int printf_char(va_list val)
+{
+	char c;
+	c = va_arg(val, int);
+	_putchar(c);
+	return (1);
+}
