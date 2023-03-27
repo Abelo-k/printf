f978c678a87df1789f6425721c752d02a429c729
Abel_k <kebedeabel34@gmail.com>
2023-03-27 07:42:02 -0700

    strlen

--- main.h
+++ main.h
@@ -36,7 +36,6 @@ int _strlen(char *s);
 int *_strcpy(char *dest, char *src);
 int _strlenc(const char *s);
 int rev_string(char *s);
-int _strlenc(const char *s);
 int printf_37(void);
 int printf_char(va_list val);
 int printf_string(va_list val);
---
+++ strlen.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * _strlen - prints length of a string
+ * @s: string
+ *
+ * Return: count
+ */
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (*s != '\0')
+	{
+		count++;
+		s++;
+	}
+
+	return (count);
+}
+/**
+ * _strlen - prints length of a string for constant char
+ * @s: string
+ *
+ * Return: count
+ */
+int _strlenc(const char *s)
+{
+	int count = 0;
+
+	while (*s != '\0')
+	{
+		count++;
+		s++;
+	}
+
+	return (count);
+}
