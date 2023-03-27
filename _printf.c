2f2174f4572935cbda11ae0d297f2e811f4a8089
Simon-Asfaw <simonasfaw587@gmail.com>
2023-03-27 11:44:15 +0000

    first try

---
+++ _printf.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _printf: prints output
+ * @format: the input format
+ * Return: 0
+ */
+
+int _printf(const char *format, ...)
+{
+	/**
+	 * we try to put out the charachters
+	 */
+
+	_putchar(...);
+	return (0);
+}
